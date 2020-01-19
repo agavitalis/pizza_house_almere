7b4060a5ed55fa248963362ad43717f3ea3b3ceb
Ogbonna Vitalis <agavitalisogbonna@gmail.com>
2020-01-19 13:55:17 +0100

    Project update

--- routes/web.php
+++ routes/web.php
@@ -1,36 +1,23 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-
 //Application routes 
 Route::group(['namespace'=>"Appp"],function(){
     
     Route::get('/', 'HomeController@index')->name('index');
-    Route::match(['GET','POST'],'/advert','IndexController@advert')->name('advert');
     
 });
 
 //User routes 
-Route::group(['namespace'=>"Appp"],function(){
+Route::group(['namespace'=>"User"],function(){
      
-    Route::match(['GET','POST'],'/advert','IndexController@advert')->name('advert');
+    Route::match(['GET','POST'],'/dashboard','DashboardController@dashboard')->name('dashboard');
     
 });
 
 //Admin routes 
-Route::group(['namespace'=>"Appp"],function(){
+Route::group(['namespace'=>"Admin"],function(){
      
-    Route::match(['GET','POST'],'/advert','IndexController@advert')->name('advert');
+    Route::match(['GET','POST'],'/admin_dashboard','IndexController@dashboard')->name('admin_dashboard');
     
 });
 
